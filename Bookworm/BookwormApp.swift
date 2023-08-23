85e84be0146ea5a99a920cf6614d63fac09b4211
Rob Ranf <rob@emiyaconsulting.com>
2023-08-23 16:42:12 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Rob Ranf on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
