7b1e70942a82c88d7df27a11d0f8f24cb8ca0ecc
Rob Ranf <rob@emiyaconsulting.com>
2023-09-06 08:58:05 -0700

    Emoji rating view

---
+++ Bookworm/Views/EmojiRatingView.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Rob Ranf on 9/5/23.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("🤮")
+        case 2:
+            Text("😒")
+        case 3:
+            Text("😉")
+        case 4:
+            Text("😌")
+        default:
+            Text("😃")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
